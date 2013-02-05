6033169bf27ba3e09b27a1b01ae63ce699f9e09d
Michael Beller <mjbeller@gmail.com>
2013-02-04 23:35:17 -0500

    initial commit

---
+++ controllers/plusc.py
@@ -0,0 +1,35 @@
+from twilio import twiml
+from twilio.rest import TwilioRestClient
+
+# coding: utf8
+# try something like
+
+def addlocation():
+    form=SQLFORM(db.t_plus_location)
+    if form.process().accepted:
+        session.flash = 'form accepted'
+        redirect(URL('viewlocations'))
+    elif form.errors:
+        response.flash = 'form has errors'
+    #else:
+    #    response.flash = 'please fill the form'
+    return dict(form=form)
+    
+def manage_locations():
+    grid=SQLFORM.smartgrid(db.t_plus_location,
+        linked_tables=['t_plus_machine'])
+    return grid
+
+def receivesms():
+    db.t_plus_sms_messages.insert(
+        f_from = request.vars['From'],
+        f_to = request.vars.To,
+        f_body = request.vars.Body)
+        
+    #sender = request.vars['From'][-10:]+'@vtext.com'
+    #mail.send([sender],'hello',request.vars.Body)
+    
+    resp = twiml.Response()
+    # resp.sms("hello")
+    
+    return str(resp) # dict()
